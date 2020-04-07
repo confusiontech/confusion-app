74b93a349f4c64a0c4e420dfa0a93058c415939f
Diego Veralli <diegoov@gmail.com>
2020-04-07 21:39:35 +0200

    Movidas categorias a servicio aparte

--- src/components/category-filter.js
+++ src/components/category-filter.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Picker } from 'react-native';
+import categoryService from '../services/category-service';
 
 export default function CategoryFilter({onOptionClick}) {
   const [selectedValue, setSelectedValue] = useState("");
@@ -12,9 +13,10 @@ export default function CategoryFilter({onOptionClick}) {
           onOptionClick(itemValue);
       }}
     >
-      <Picker.Item label="All" value="" />
-      <Picker.Item label="Health" value="health" />
-      <Picker.Item label="Music" value="music" />
+    <Picker.Item label="Todas" value="" key="" />    
+    {categoryService.getAllCategories().map(item =>
+      <Picker.Item label={item.name} value={item.id} key={item.id} />
+    )}
     </Picker>
   );
 }
---
+++ src/services/category-service.js
@@ -0,0 +1,19 @@
+class CategoryService {
+    getAllCategories() {
+        return [
+            { id: "arts", name: "Artes Escénicas" },
+            { id: "audiovisual", name: "Audiovisual" },
+            { id: "craftwork", name: "Artesanía" },
+            { id: "gastronomy", name: "Gastronomía" },
+            { id: "health", name: "Salud y Bienestar" },
+            { id: "literature", name: "Literatura" },
+            { id: "music", name: "Música" },
+            { id: "other", name: "Otros" },
+            { id: "street_art", name: "Arte Urbano" },
+            { id: "visual", name: "Artes Visuales" }
+        ];
+    }
+}
+
+
+export default new CategoryService();
