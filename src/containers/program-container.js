34b05e3728a254e3bc174b92e4c8d29101088ad5
Diego Veralli <diegoov@gmail.com>
2020-04-28 20:45:43 +0200

    Movida petición de programa a orfheo a servicio de backend y añadido proxy para cors.

--- src/containers/program-container.js
+++ src/containers/program-container.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect,useRef } from 'react';
 import Program from '../components/program';
 import CategoryFilter from '../components/category-filter';
+import BackendService from '../services/backend-service';
 import { View } from 'react-native';
 
 import { Platform } from 'react-native';
@@ -10,30 +11,12 @@ const ProgramContainer = () => {
   const [shows, setShows] = useState([]);
   const allShows = useRef([]);
   
-  useEffect(() => {    
-    async function fetchData() {
-      console.log('fetching')
-      const cors_url = 'https://cors-anywhere.herokuapp.com/'
-      const orfheo_url = 'https://www.orfheo.org/search/results_program'
-      const request_url = Platform.OS === 'web' ? `${cors_url}${orfheo_url}` : orfheo_url
-      try {
-        let response = await fetch(request_url, {          
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-          },
-          body: "event_id=3cb8e68a-b03f-4a7d-b714-42a7ca56870a&date=&lang=es"    
-        });
-
-        let responseJson = await response.json();
-        allShows.current = responseJson['program'];
-        setShows(responseJson['program']);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    if (allShows.current.length == 0) fetchData();
-  });
+  useEffect(() => {
+    BackendService.fetchProgram().then(function(response) {
+      allShows.current = response['program'];
+      setShows(response['program']);
+    });
+  }, []);
 
   const onCategoryFilterClick = (selectedCategory) => {
     if (selectedCategory) {
--- src/services/backend-service.js
+++ src/services/backend-service.js
@@ -1,25 +1,30 @@
 import { Platform } from 'react-native';
 
 class BackendService {
-
-  async fetchData() {
-    const cors_url = 'https://cors-anywhere.herokuapp.com/'
-    const orfheo_url = 'https://www.orfheo.org/search/results_program'
-    const request_url = Platform.OS === 'web' ? `${cors_url}${orfheo_url}` : orfheo_url
-    try {
-      let response = await fetch(request_url, {          
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
-        body: "event_id=3cb8e68a-b03f-4a7d-b714-42a7ca56870a&date=&lang=es"    
-      });
-
-      return await response.json();
-     
-    } catch (error) {
-      console.error(error);
+  constructor() {
+    if (Platform.OS === 'web') {
+      this.urlBase = 'http://localhost:8010/proxy/';
+    } else {
+      this.urlBase = 'https://www.orfheo.org/';
     }
+  }
+
+  getUrl(path) {
+    return this.urlBase + path;
+  }
+  
+  async fetchProgram() {
+    const programUrl = this.getUrl("search/results_program");
+
+    let response = await fetch(programUrl, {          
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: "event_id=3cb8e68a-b03f-4a7d-b714-42a7ca56870a&date=&lang=es"    
+    });
+
+    return response.json();
   }
 }
 
