7f63c24b22cfe1b676c2e8a1b633b212c7ec02d3
Diego Veralli <diegoov@gmail.com>
2020-07-26 23:28:52 +0200

    Añadido mapa a vista de evento.

--- src/containers/show-container.js
+++ src/containers/show-container.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Dimensions, Text, View, Linking } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker, Callout } from 'react-native-maps';
 import { getParticipantCategory, getPublic, getAddress } from '../helpers/program-helpers';
 import { capitalize } from '../helpers/text-helpers';
 import { getEsMoment } from '../helpers/date-helpers';
@@ -9,6 +11,18 @@ const ShowContainer = ({ route }) => {
   const momentDate = getEsMoment(show.date);
   const momentStartTime = getEsMoment(parseInt(show.time[0]));
   const momentEndTime = getEsMoment(parseInt(show.time[1]));
+  const latitude = parseFloat(show.address.location.lat);
+  const longitude = parseFloat(show.address.location.lng);
+
+  let marker = null;
+
+  const setMarkerRef = (ref) => {
+    marker = ref;
+  };
+
+  const mapUrl = `https://www.google.com/maps/place/${latitude},${longitude}/@${latitude},${longitude},15z`;
+  const openMapUrl = ()=> Linking.openURL(mapUrl);
+
   return (
       <View>
         <Text>{show.participant_name}</Text>
@@ -24,8 +38,38 @@ const ShowContainer = ({ route }) => {
         <Text>{getPublic(show)}</Text>
         <Text>{show.order} {show.host_name}</Text>
         <Text>{getAddress(show)}</Text>
+
+        <MapView style={styles.mapStyle}
+          initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.0022,
+            longitudeDelta: 0.002
+          }}
+    onLayout={() => marker.showCallout()} >
+          <Marker coordinate={{latitude: latitude, longitude: longitude}} onPress={openMapUrl} ref={setMarkerRef}>
+            <Callout onPress={openMapUrl}>
+              <View>
+                <Text>{`${show.order}: ${show.host_name}`}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        </MapView>
       </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: 300,
+    height: 300
+  },
+});
 
 export default ShowContainer;
