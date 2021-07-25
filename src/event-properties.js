5c6451766014b84b06dc4cf000b10bdf9d0fd005
pietrogllcw <pietrogll@aol.com>
2021-07-25 17:51:35 +0200

    Definido fichero con las propriedades del evento

---
+++ src/event-properties.js
@@ -0,0 +1,15 @@
+export const EVENT_ID = '3cb8e68a-b03f-4a7d-b714-42a7ca56870a';
+export const EVENT_DATES = ['2019-10-19', '2019-10-20'];
+// export const EVENT_ID = '79a00aa4-2704-4581-9f2e-84b6549668b1'; // 2021
+// export const EVENT_DATES = ['2021-10-16', '2021-10-17']; // 2021
+export const EVENT_CATEGORIES = [
+  ['0', 'Artes Escénicas'],
+  ['1', 'Arte Urbano'],
+  ['2', 'Audiovisual'],
+  ['3', 'Exposición'],
+  ['4', 'Música'],
+  ['5', 'Otros'],
+  ['6', 'Poesía'],
+  ['7', 'Talleres / Charlas'],
+  ['8', 'Instalaciones Urbanas']
+];
